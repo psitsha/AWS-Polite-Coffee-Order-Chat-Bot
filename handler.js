2837c1c1f5c5ac96a36cedb9369e69478a8ee063
n0364146 <polite.sitsha@liberty-it.co.uk>
2019-03-04 14:57:53 +0000

    first commit

---
+++ handler.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const dispatch = require("./dispatch");
+
+module.exports.intents = (event, context, callback) => {
+  try {
+    console.log(`event.bot.name=${event.bot.name}`);
+    dispatch(event, response => callback(null, response));
+  } catch (err) {
+    callback(err);
+  }
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
